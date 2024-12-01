2514a2b15e5b17e6d29bb53b56500ec7ce1808eb
Sachin Gupta <thesachin4545@gmail.com>
2024-12-02 04:24:46 +0530

    Interview

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -12,6 +12,18 @@ function App() {
   const addValue = () => {
     // console.log("clicked",counter);
   //  counter = counter + 1
+
+  // setCounter( counter + 1)
+  // setCounter( counter + 1)
+  // setCounter( counter + 1)
+  // setCounter( counter + 1)
+  // or
+  // if we did this then we get 19
+  // setCounter(prevCounter => prevCounter + 1)
+  // setCounter(prevCounter => prevCounter + 1)
+  // setCounter(prevCounter => prevCounter + 1)
+  // setCounter(prevCounter => prevCounter + 1)
+
   if(counter == 20){
     setCounter(counter = 20)
   }
