c07598079079b81f3c43c5a8cd2fcfd5b106f90b
Sachin Gupta <thesachin4545@gmail.com>
2025-04-09 01:49:17 +0530

    done with context and local storage

--- 08minicontext/src/context/UserContextProvider.jsx
+++ 08minicontext/src/context/UserContextProvider.jsx
@@ -5,7 +5,7 @@ const UserContextProvider = ({children}) => {
   const [user, setUser] = React.useState(null)
 return (
   <UserContext.Provider value={{user, setUser}}>
-   {children}
+   {children} 
   </UserContext.Provider>
 
    )
