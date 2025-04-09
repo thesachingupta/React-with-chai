d58f9135d224d1d67f89da1c95ea17692cb0d67c
Sachin Gupta <thesachin4545@gmail.com>
2025-04-10 03:48:23 +0530

    redux toolkit project

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react"; // ✅ Add this line
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+
+  return (
+    <>
+   <h1>ji</h1>
+   <AddTodo/>
+   <Todos/>
+    </>
+  )
+}
+
+export default App
